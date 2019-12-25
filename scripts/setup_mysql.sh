015892c4c5614cb9c6c54886859579d58f3d28b0
jack <wjl198435@@163.com>
2019-12-25 15:23:39 +0800

    添加mysql 创建hass 用户 hass_db 脚步 telegrah mqtt ip 改为localhost

--- scripts/setup_mysql.sh
+++ scripts/setup_mysql.sh
@@ -10,7 +10,16 @@ echo "CREATE DATABASE grafana" | mysql --default-character-set=utf8
 echo "CREATE USER '${MYSQL_GRAFANA_USER}' IDENTIFIED BY '${MYSQL_GRAFANA_PW}'" | mysql --default-character-set=utf8
 echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_GRAFANA_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
 
+echo "CREATE DATABASE hass_db" | mysql --default-character-set=utf8
+echo "CREATE USER 'hass' IDENTIFIED BY 'hass'" | mysql --default-character-set=utf8
+echo "GRANT ALL PRIVILEGES ON *.* TO 'hass'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+
 echo "=> Stopping MySQL ..."
 /etc/init.d/mysql stop
 
 exit 0
+
+### root 登陆 说明
+## 1. 登陆容器 sudo  docker exec -it mysql  /bin/bash
+## 2. mysql -u root -p
+## 直接回车
